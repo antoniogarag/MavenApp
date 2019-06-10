9b9238b1f9ef70e5a8ff08da1f14f9dd70d6492f
admin <admin@MBP-de-admin.home>
2019-06-10 23:16:33 +0200

    first commit

---
+++ src/main/java/antonio/garcia/MavenApp/App.java
@@ -0,0 +1,13 @@
+package antonio.garcia.MavenApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
